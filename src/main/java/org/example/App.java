9ad2e944a93c00cefac64b5e827b14f60c2966d1
chrissy-buzz <christinapersaud98@gmail.com>
2021-09-11 19:56:34 -0700

    $ git config --global user.email "christinapersaud98@gmail.com"
            $ git config --global user.name "Chrissy-buzz"
    
            $ git config --list

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Christina Persaud
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
