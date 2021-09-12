977c5be437943b16bab29b2feaee1cf74730cf80
chrissy-buzz <christinapersaud98@gmail.com>
2021-09-12 10:09:39 -0700

    $ git config --global user.email "christinapersaud98@gmail.com" $ git config --global user.name "Chrissy-buzz"  $ git config --list

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,4 +1,5 @@
 package org.example;
+import java.util.Scanner;  // Import the Scanner class
 
 /*
  *  UCF COP3330 Fall 2021 Assignment 1 Solution
@@ -7,8 +8,11 @@ package org.example;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("What is your name?");
+
+        String Name = myObj.nextLine();  // Read user input
+        System.out.println("Hello," + Name + ", nice to meet you!");  // Output user input
     }
 }
